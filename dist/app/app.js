59ce4ce32129a92069cbd8ad11ac6b8dc5a6d70b
Pierce Issah <PJIceskull2003@gmail.com>
2023-11-05 19:30:11 -0500

    initlize files to git repo

---
+++ dist/app/app.js
@@ -0,0 +1,40 @@
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  console.log(hashTag + " " + pageID);
+
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    }).fail(function () {
+      console.log(pageID, "page not found");
+      alert(pageID + " page not found.");
+    });
+  } else {
+    $.get(`pages/login.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    }).fail(function () {
+      alert(pageID + " page not found.");
+    });
+  }
+}
+
+function initURLListener() {
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+}
+
+function initListeners() {
+  // Hamburger Menu
+  // Using Jquery
+  $(".hamburger-menu").on("click", function () {
+    $(this).toggleClass("open");
+  });
+}
+
+$(document).ready(function () {
+  initURLListener();
+  initListeners();
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Jungle Cook - Login
+
+Pierce Issah
+
+## Website Link
+
+<!-- Web 4 Links -->
+
+<!-- Link to HW-Seven -->
+
+[Web-4 link](https://in-info-web4.informatics.iupui.edu/~pissah/N315/homework-seven)
+
+[Github Pages](https://pjiceskull.github.io/Jungle-Cook/)
+
+## About Project
+
+For this project, I will be making the login page of the Jungle Cook website work with Firebase. This feature will be dynamic as you must let a user sign up and sign in with an email and a password. They must also see an updated navigation menu when they are logged in.
