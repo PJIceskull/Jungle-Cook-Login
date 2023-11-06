1a42f1caf1cd50668e09c2669e1fee33a2abc341
Pierce Issah <PJIceskull2003@gmail.com>
2023-11-05 19:51:35 -0500

    create file for url route

---
+++ src/route.js
@@ -0,0 +1,22 @@
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  console.log(hashTag + " " + pageID);
+
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    }).fail(function () {
+      console.log(pageID, "page not found");
+      alert(pageID + " page not found.");
+    });
+  } else {
+    $.get(`pages/login.html`, function (data) {
+      //   console.log("data " + data);
+      $("#app").html(data);
+    }).fail(function () {
+      alert(pageID + " page not found.");
+    });
+  }
+}
