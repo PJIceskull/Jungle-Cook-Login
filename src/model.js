830162e079d3448dc6a8149e5be9dbf24c21a080
Pierce Issah <PJIceskull2003@gmail.com>
2023-11-19 12:28:37 -0500

    replaced route function

--- src/model.js
+++ src/model.js
@@ -8,42 +8,48 @@ export function changePage(pageName) {
   $(".login-Form .signin-Form div").off("click", "**");
   $(".login-Form .signin-Form .signinBTN").off("click", "**");
 
-  if (pageName == "") {
-    $.get(`pages/${pageName}.html`, function (data) {
-      $("#app").html(data);
-    }).fail(function (error) {
-      console.log("Error: ", error);
-    });
-  } else if (pageName == "login") {
-    $.get(`pages/${pageName}.html`, function (data) {
-      $("#app").html(data);
-      addFormListener();
-    }).fail(function (error) {
-      console.log("Error: ", error);
-    });
-  } else {
-    $.get(`pages/${pageName}.html`, function (data) {
-      $("#app").html(data);
-    }).fail(function (error) {
-      console.log("Error: ", error);
-    });
-  }
-}
-
-function changeRoute() {
-  let hashTag = window.location.hash;
-  let pageID = hashTag.replace("#", "");
-  //   console.log(hashTag + ' ' + pageID);
-
   if (pageID != "") {
-    $.get(`pages/pageID/pageID.html`, function (data) {
-      console.log("data " + data);
+    $.get(`pages/${pageID}.html`, function (data) {
+      // console.log("data " + data);
+      console.log("data: " + pageID);
       $("#app").html(data);
+      // Set Listerners on other pages
+
+      // Set Listeners if on login.html page
+      if (pageID == "login") {
+        $.get(`pages/${pageID}.html`, function (data) {
+          $("#app").html(data);
+          addFormListener();
+        });
+      }
+    }).fail(function (error) {
+      console.log("Error: ", error);
     });
   } else {
-    $.get(`pages/home/home.html`, function (data) {
-      console.log("data " + data);
+    $.get(`pages/home.html`, function (data) {
+      // console.log("data " + data);
+      console.log(pageID);
       $("#app").html(data);
+    }).fail(function (error) {
+      console.log("Error: ", error);
     });
   }
 }
+
+// function changeRoute() {
+//   let hashTag = window.location.hash;
+//   let pageID = hashTag.replace("#", "");
+//   //   console.log(hashTag + ' ' + pageID);
+
+//   if (pageID != "") {
+//     $.get(`pages/pageID/pageID.html`, function (data) {
+//       console.log("data " + data);
+//       $("#app").html(data);
+//     });
+//   } else {
+//     $.get(`pages/home/home.html`, function (data) {
+//       console.log("data " + data);
+//       $("#app").html(data);
+//     });
+//   }
+// }
