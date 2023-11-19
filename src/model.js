c875dcfcd7a615bce71fe836f5c1c0a852ec5b95
Pierce Issah <PJIceskull2003@gmail.com>
2023-11-19 12:55:04 -0500

    removed commented code

--- src/model.js
+++ src/model.js
@@ -35,21 +35,3 @@ export function changePage(pageName) {
     });
   }
 }
-
-// function changeRoute() {
-//   let hashTag = window.location.hash;
-//   let pageID = hashTag.replace("#", "");
-//   //   console.log(hashTag + ' ' + pageID);
-
-//   if (pageID != "") {
-//     $.get(`pages/pageID/pageID.html`, function (data) {
-//       console.log("data " + data);
-//       $("#app").html(data);
-//     });
-//   } else {
-//     $.get(`pages/home/home.html`, function (data) {
-//       console.log("data " + data);
-//       $("#app").html(data);
-//     });
-//   }
-// }
